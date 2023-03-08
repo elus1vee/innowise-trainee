af0cd75fa30e9f1f218f799e2d75692ef7f7d8f3
Aliaksandr Yablonski <sasha432301@gmail.com>
2023-03-08 16:20:24 +0300

    added variables,mixin,dark-theme

---
+++ Task_2/script/index.js
@@ -0,0 +1,21 @@
+let link = document.getElementById("theme-link");
+let btn = document.getElementById("button-toggle");
+let fileName = location.pathname.match(/[^/]*$/)[0].split(".")[0];
+if (fileName == "index") {
+  fileName = "home-page";
+}
+btn.addEventListener("click", () => {
+  if (fileName == "home-page") {
+    link.href = `./styles/css/${fileName}${
+      link.getAttribute("href") == `./styles/css/${fileName}.css`
+        ? "-dark.css"
+        : ".css"
+    }`;
+  } else {
+    link.href = `../styles/css/${fileName}${
+      link.getAttribute("href") == `../styles/css/${fileName}.css`
+        ? "-dark.css"
+        : ".css"
+    }`;
+  }
+});
