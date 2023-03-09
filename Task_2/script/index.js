22c1fe11a8d9ce50038ae3efb99b15e188462232
Aliaksandr Yablonski <sasha432301@gmail.com>
2023-03-10 00:08:10 +0300

    added video-tag, fixed img-height

--- Task_2/script/index.js
+++ Task_2/script/index.js
@@ -1,21 +1,30 @@
+let currentTheme = localStorage.getItem("theme");
 let link = document.getElementById("theme-link");
 let btn = document.getElementById("button-toggle");
 let fileName = location.pathname.match(/[^/]*$/)[0].split(".")[0];
 if (fileName == "index" || fileName == undefined || fileName == "") {
   fileName = "home-page";
 }
+
+link.href = `.${
+  fileName == "home-page" ? "" : "."
+}/styles/css/${fileName}${currentTheme} `;
+
 btn.addEventListener("click", () => {
   if (fileName == "home-page") {
     link.href = `./styles/css/${fileName}${
-      link.getAttribute("href") == `./styles/css/${fileName}.css`
-        ? "-dark.css"
-        : ".css"
+      currentTheme == ".css" ? "-dark.css" : ".css"
     }`;
   } else {
     link.href = `../styles/css/${fileName}${
-      link.getAttribute("href") == `../styles/css/${fileName}.css`
-        ? "-dark.css"
-        : ".css"
+      currentTheme == ".css" ? "-dark.css" : ".css"
     }`;
+  }
+  if (currentTheme == ".css") {
+    localStorage.setItem("theme", "-dark.css");
+    currentTheme = "-dark.css";
+  } else {
+    localStorage.setItem("theme", ".css");
+    currentTheme = ".css";
   }
 });
