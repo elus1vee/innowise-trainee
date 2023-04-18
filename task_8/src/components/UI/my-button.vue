d924742278967a202930753d3815f9f5befb1bc5
Aliaksandr Yablonski <sasha432301@gmail.com>
2023-04-18 19:18:06 +0300

    deploy for vercel

--- task_8/src/components/UI/my-button.vue
+++ task_8/src/components/UI/my-button.vue
@@ -11,11 +11,11 @@ export default {
 <style scoped>
     .button{
         background: inherit;
-        border: 2px solid rgb(255, 170, 13);
+        border: 2px solid rgb(254, 170, 13);
         border-radius: 10px;
         padding: 3px 5px;
         font-weight: 800;
-        color: rgb(255, 170, 13);
+        color: rgb(254, 170, 13);
     }
     .button:hover{
         background-color: rgb(255, 239, 193);
